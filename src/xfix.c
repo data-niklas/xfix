7ee7788bbabc8cf49bfc64754947f5ee52f78b24
Niklas Loeser <51879435+data-niklas@users.noreply.github.com>
2021-01-19 23:04:30 +0100

    first methods

--- src/xfix.c
+++ src/xfix.c
@@ -1,1 +1,24 @@
+#include "Xlib.h"
+#include "../lib/bstrlib.h"
+
 #include "xfix.h"
+
+
+void xfix_flush(xfix_display * display){
+    XFlush(display->display);
+}
+
+void xfix_fill_display(xfix_display * display){
+    display->root = XRootWindow(display->display, XDefaultScreen(display->display));
+}
+void xfix_init_display(xfix_display * display){
+    display->display = XOpenDisplay(NULL);
+    xfix_fill_display(display);
+}
+void xfix_init_display_by_name(xfix_display * display, bstring name){
+    display->display = XOpenDisplay(name->data);
+    xfix_fill_display(display);
+}
+void xfix_free_display(xfix_display * display){
+    XCloseDisplay(display->display);
+}
--- src/xfix.h
+++ src/xfix.h
@@ -1,4 +1,31 @@
 #ifndef XFIX_H
 #define XFIX_H
 
+#include "Xlib.h"
+
+#include "../lib/bstrlib.h"
+
+typedef struct xfix_display{
+    Display * display;
+    Window root;
+    #ifdef XIMERAMA_
+    int num_screens;
+    #endif
+} xfix_display;
+
+typedef struct xfix_window {
+    Window window;
+} xfix_window;
+
+
+
+/* ---------------------------------------------------------------- */
+void xfix_flush(xfix_display * display);
+
+void xfix_fill_display(xfix_display * display);
+void xfix_init_display(xfix_display * display);
+void xfix_init_display_by_name(xfix_display * display, bstring name);
+void xfix_free_display(xfix_display * display);
+
+
 #endif
